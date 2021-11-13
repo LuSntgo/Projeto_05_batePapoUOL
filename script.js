6f0ffad4ffa69b963f8c0eba67ae15a3427e88a3
LuSntgo <lu.sntgo@gmail.com>
2021-11-12 21:24:40 -0300

    Adicionada tela de login

---
+++ script.js
@@ -0,0 +1,48 @@
+let userName = ""; 
+
+//criar uma tela de login
+
+function login () {
+  
+  } 
+      
+login();
+
+
+// const userLogin = (('https://mock-api.driven.com.br/api/v4/uol/participants'), username);
+
+/*
+
+
+//quando a resposta for sucesso, fazer: 
+function getSucess(response) {
+    const mensage = response.data;
+}
+
+const promise = axios.get("")
+
+
+function getError(error) {
+    console.log(error.response)
+}
+
+//quando a resposta der erro, ele executa a função de exibir erro
+promise.catch(getError);
+
+
+//enviar um objeto com os dados coletados para API, exemplo
+const album = document.querySelector(".musica").value 
+//ou 
+const musicas = {
+    album: inputAlbum.value,
+    genero: [inputGenero.value] //é um array
+}
+
+//  envia os dados coletados para o axios
+axios.post(("url"), musica)
+
+
+// para cada requisição, envia um catch / then
+
+// fazer função para atualziar a pagina (para mostrar os dados enviados)
+*/
