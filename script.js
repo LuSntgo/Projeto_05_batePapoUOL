cc7dce5c55e5966c8fb432664be68acb1e419d30
LuSntgo <lu.sntgo@gmail.com>
2021-11-13 21:54:02 -0300

    Envio de nome do usuaáio para servidor e mantendo conexão

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const allMessages = "";
+let userName = { name: ""}
+
+const login = document.querySelector(".loginScreen");
+const loading = document.querySelector(".loading");
+
+function loginScreen() {
+    const nameInput = document.querySelector(".nickname").value;
+    userName = {name: nameInput};
+    login.classList.add("hide");
+    loading.classList.remove("hide");
+
+    setTimeout(startChat,3000);
+    promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants",userName );
+    
+    promise.then(startChat);
+    promise.catch(error);
+}
+
+
+function startChat(){
+    const chatList = document.querySelector(".chat");
+    loading.classList.add("hide");
+    chatList.classList.remove("hide");
+    setInterval(conected,5000); 
+}
+function conected () {
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/status",userName);
+}
+
 //função para exibir mensagens colocadas no chat
 function sucess (response){
 const message = response.data;
@@ -12,6 +42,11 @@ for (let i = 0; i < message.length; i++) {
 function error(erro){
     console.log(erro.response);
 }
+
+/*function statusType(response){
+    if (type === "status")
+
+}*/
 //criar uma tela de login
 
 
@@ -20,17 +55,16 @@ function reloadPage () {
 
     promise.then(sucess); 
     promise.catch(error);
+    
       } 
 reloadPage();
 
-      /*
+
 //enter in the chat with enter
 function pressEnter(){
     const keyEnter = event.keyCode;
-    const clickButton = document.querySelector(".button")
-    
+      
     if (keyEnter === 13) {
-        startChat();
+        loginScreen();
     }
 }
-*/
